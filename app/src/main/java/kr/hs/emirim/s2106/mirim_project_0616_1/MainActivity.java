3363f48b58461ef004533c7091db26f13391d6db
yday <yangdayeon2@gmail.com>
2022-06-16 19:32:22 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/s2106/mirim_project_0616_1/MainActivity.java
@@ -0,0 +1,28 @@
+package kr.hs.emirim.s2106.mirim_project_0616_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.text.style.TabStopSpan;
+import android.widget.TabHost;
+
+public class MainActivity extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost host = getTabHost();
+        TabHost.TabSpec spec1 = host.newTabSpec("tab1").setIndicator("팡푸");
+        spec1.setContent(R.id.tab1);
+        host.addTab(spec1);
+        TabHost.TabSpec spec2 = host.newTabSpec("tab2").setIndicator("박서함");
+        spec2.setContent(R.id.tab2);
+        host.addTab(spec2);
+        TabHost.TabSpec spec3 = host.newTabSpec("tab3").setIndicator("박재찬");
+        spec3.setContent(R.id.tab3);
+        host.addTab(spec3);
+    }
+}
